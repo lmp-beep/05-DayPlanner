91238fbe636a7904d698898773043205aa40b06d
Lisa Pessin <lisapessin@gmail.com>
2021-03-20 09:21:35 -0400

    tutor helped me clean up my ids and classes

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 https://lmp-beep.github.io/05-DayPlanner/
 
+Special thanks to my tutor Dennis Itua for taking the time to emplain things to me in a way that I understand.
+
 This is a simple calendar app for scheduling appointments during working hours. The user can add text to each time block and save it by clicking the lock button. The info will be stored in local storage and will not be deleted upon refresh or reload. The colors of each time block vary based on the current time. Past hours appear grey, future hours appear green, and the current hour appears red.
 
 ![screencapture-127-0-0-1-5500-2021-03-18-14_35_10](https://user-images.githubusercontent.com/77073582/111678912-2b6a7100-87f7-11eb-9f80-1c8e68a9bfce.png)
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 
 // document.ready
-$(function() {
+$(document).ready(function() {
     console.log("ready!");
+
 
 
 // Displays current date in header
@@ -14,54 +15,80 @@ $("#currentTime").text(todayTime.format("LT"));
 
 // function to change color of each time block based on current time
 $("textarea").each(function () {
-    // variable to set each rows "name"
-    var name = parseInt($(this).attr("name"));
-    // variable for "now" time
-    var now = parseInt(moment().format("HH"));
-    if (name < now) {
+    // variable to set each textearea's name from the HTML
+    var hour = parseInt($(this).attr("name"));
+    // variable for current time - HH didplays time as a two digit whole number 00 01 02 . . . 22 23 24
+    var nowTime = parseInt(moment().format("HH"));
+    if (hour < nowTime) {
         $(this).addClass("past");
-    } else if (name > now) {
+    } else if (hour > nowTime) {
         $(this).addClass("future");
     } else {
         $(this).addClass("present")
     }
 })
 
-});
 
 
 // variables for each time block
-var $text9AM = $("#text9AM");
-var $text10AM = $("#text10AM");
-var $text11AM = $("#text11AM");
-var $text12PM = $("#text12PM");
-var $text1PM = $("#text1PM");
-var $text2PM = $("#text2PM");
-var $text3PM = $("#text3PM");
-var $text4PM = $("#text4PM");
-var $text5PM = $("#text5PM");
+var text9 = $(".9text");
+var text10 = $(".10text");
+var text11 = $(".11text");
+var text12 = $(".12text");
+var text13 = $(".13text");
+var text14 = $(".14text");
+var text15 = $(".15text");
+var text16 = $(".16text");
+var text17 = $(".17text");
 
 // setting local storage
 $("button").on("click", function() {
-    console.log("I just got clicked!");
-    localStorage.setItem("9AM", ($text9AM.val()));
-    localStorage.setItem("10AM", ($text10AM.val()));
-    localStorage.setItem("11AM", ($text11AM.val()));
-    localStorage.setItem("12PM", ($text12PM.val()));
-    localStorage.setItem("1PM", ($text1PM.val()));
-    localStorage.setItem("2PM", ($text2PM.val()));
-    localStorage.setItem("3PM", ($text3PM.val()));
-    localStorage.setItem("4PM", ($text4PM.val()));
-    localStorage.setItem("5PM", ($text5PM.val()));
-    });
+    console.log("I just got clicked")
+    localStorage.setItem("9AM", (text9.val()));
+    localStorage.setItem("10AM", (text10.val()));
+    localStorage.setItem("11AM", (text11.val()));
+    localStorage.setItem("12PM", (text12.val()));
+    localStorage.setItem("1PM", (text13.val()));
+    localStorage.setItem("2PM", (text14.val()));
+    localStorage.setItem("3PM", (text15.val()));
+    localStorage.setItem("4PM", (text16.val()));
+    localStorage.setItem("5PM", (text17.val()));
+})
 
     //recovering localstorage
-    $("#text9AM").append(localStorage.getItem("9AM"));
-    $("#text10AM").append(localStorage.getItem("10AM"));
-    $("#text11AM").append(localStorage.getItem("11AM"));
-    $("#text12PM").append(localStorage.getItem("12PM"));
-    $("#text1PM").append(localStorage.getItem("1PM"));
-    $("#text2PM").append(localStorage.getItem("2PM"));
-    $("#text3PM").append(localStorage.getItem("3PM"));
-    $("#text4PM").append(localStorage.getItem("4PM"));
-    $("#text5PM").append(localStorage.getItem("5PM"));
+    $("#9AM .9text").val(localStorage.getItem("9AM"));
+    $("#10AM .10text").append(localStorage.getItem("10AM"));
+    $("#11AM .11text").append(localStorage.getItem("11AM"));
+    $("#12PM .12text").append(localStorage.getItem("12PM"));
+    $("#1PM .13text").append(localStorage.getItem("1PM"));
+    $("#2PM .14text").append(localStorage.getItem("2PM"));
+    $("#3PM .15text").append(localStorage.getItem("3PM"));
+    $("#4PM .16text").append(localStorage.getItem("4PM"));
+    $("#5PM .17text").append(localStorage.getItem("5PM"));
+
+
+});
+
+
+// trying another way of saving to local storage
+// couldn't get it to work
+
+// $("saveBtn").on("click", function() {
+    //     console.log("I just got clicked!");
+    //     var storage = $(this).siblings(".text").val();
+    //     var data = $(this).parent().attr("id");
+    //     localStorage.setItem(data, storage);
+
+    // $("#9AM .9text").val(localStorage.getItem("9AM")); 
+    // $("#10AM .10text").val(localStorage.getItem("10AM")); 
+    // $("#11AM .11text").val(localStorage.getItem("11AM")); 
+    // $("#12AM .12text").val(localStorage.getItem("12PM")); 
+    // $("#1PM .13text").val(localStorage.getItem("1PM")); 
+    // $("#2PM .14text").val(localStorage.getItem("2PM")); 
+    // $("#3PM .15text").val(localStorage.getItem("3PM")); 
+    // $("#4PM .16text").val(localStorage.getItem("4PM")); 
+    // $("#5PM .17text").val(localStorage.getItem("5PM")); 
+
+
+
+    
